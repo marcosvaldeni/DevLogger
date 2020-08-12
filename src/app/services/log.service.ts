5f5f0bc50964962ba51132a7b76356c45708a36a
Marcos Lucas <marcosvaldeni@gmail.com>
2020-08-12 11:35:04 +0100

    implementing Local Storage

--- src/app/services/log.service.ts
+++ src/app/services/log.service.ts
@@ -35,7 +35,15 @@ export class LogService {
   }
 
   getLogs(): Observable<Log[]> {
-    return of(this.logs);
+    if(localStorage.getItem('logs') === null) {
+      this.logs = [];
+    } else {
+      this.logs = JSON.parse(localStorage.getItem('logs'));
+    }
+
+    return of(this.logs.sort((a, b) => {
+      return b.date = a.date;
+    }));
   }
 
   setFormLog(log: Log) {
@@ -44,7 +52,9 @@ export class LogService {
 
   addLog(log: Log) {
     this.logs.unshift(log);
-    console.log(this.logs);
+
+    // Add to local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   updateLog(log: Log) {
@@ -54,6 +64,9 @@ export class LogService {
       }
     });
     this.logs.unshift(log);
+
+    // Update local storage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   deleteLog(log) {
@@ -62,6 +75,9 @@ export class LogService {
         this.logs.splice(index, 1);
       }
     });
+
+    // Delete from localstorage
+    localStorage.setItem('logs', JSON.stringify(this.logs));
   }
 
   clearState() {
